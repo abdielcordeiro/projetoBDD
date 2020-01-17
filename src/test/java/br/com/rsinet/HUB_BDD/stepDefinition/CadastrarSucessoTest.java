f5a7ab2bde9b535c0c48a42c77d33323baf37533
Abdiel Cordeiro <abdiel.boaz1@gmail.com>
2020-01-17 16:02:29 -0300

    Commit das ultimas atualizações

--- src/test/java/br/com/rsinet/HUB_BDD/stepDefinition/CadastrarSucessoTest.java
+++ src/test/java/br/com/rsinet/HUB_BDD/stepDefinition/CadastrarSucessoTest.java
@@ -1,27 +1,54 @@
 package br.com.rsinet.HUB_BDD.stepDefinition;
 
-import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Então;
 import cucumber.api.java.pt.Quando;
 
 public class CadastrarSucessoTest {
-	@Dado("^O usuário esta na pagina principal$")
-	public void o_usuário_esta_na_pagina_principal() throws Throwable {
+
+	@Quando("^Preenche o seu usuário de login <username>$")
+	public void preenche_o_seu_usuário_de_login_username() throws Throwable {
 	}
 
-	@Quando("^Navega para o login$")
-	public void navega_para_o_login() throws Throwable {
+	@Quando("^Preenche a sua senha <password>$")
+	public void preenche_a_sua_senha_password() throws Throwable {
 	}
 
-	@Quando("^Aperta no botao de cadastrar novo usuário$")
-	public void aperta_no_botao_de_cadastrar_novo_usuário() throws Throwable {
+	@Quando("^Preenche a confirmação da senha <password>$")
+	public void preenche_a_confirmação_da_senha_password() throws Throwable {
 	}
 
-	@Quando("^Preenche todas as informações$")
-	public void preenche_todas_as_informações() throws Throwable {
+	@Quando("^Preenche o seu email <email>$")
+	public void preenche_o_seu_email_email() throws Throwable {
 	}
 
-	@Então("^volta para janela principal já logado$")
-	public void volta_para_janela_principal_já_logado() throws Throwable {
+	@Quando("^Preenche o primeiro nome <fristname>$")
+	public void preenche_o_primeiro_nome_fristname() throws Throwable {
+	}
+
+	@Quando("^Preenche o ultimo nome <lastname>$")
+	public void preenche_o_ultimo_nome_lastname() throws Throwable {
+	}
+
+	@Quando("^Preenche o número de telefone <phonenumber>$")
+	public void preenche_o_número_de_telefone_phonenumber() throws Throwable {
+	}
+
+	@Quando("^Preenche a sua cidade <city>$")
+	public void preenche_a_sua_cidade_city() throws Throwable {
+	}
+
+	@Quando("^Preenche o seu endereço <address>$")
+	public void preenche_o_seu_endereço_address() throws Throwable {
+	}
+
+	@Quando("^Preenche o seu estado <state>$")
+	public void preenche_o_seu_estado_state() throws Throwable {
+	}
+
+	@Quando("^Preenche o número do seu CEP <cep>$")
+	public void preenche_o_número_do_seu_CEP_cep() throws Throwable {
+	}
+
+	@Quando("^Clica no botão de registrar$")
+	public void clica_no_botão_de_registrar() throws Throwable {
 	}
 }
